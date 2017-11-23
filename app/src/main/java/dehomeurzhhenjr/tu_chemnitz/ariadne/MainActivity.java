28574ad91b083a9cc6b212f6e8da82c60ec634d1
Henje <henje.richter@gmail.com>
2017-11-23 13:52:12 +0100

    Project initialized

---
+++ app/src/main/java/dehomeurzhhenjr/tu_chemnitz/ariadne/MainActivity.java
@@ -0,0 +1,15 @@
+package dehomeurzhhenjr.tu_chemnitz.ariadne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Test line for GitHub repository push
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
